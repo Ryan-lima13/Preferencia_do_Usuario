3cdc6cf1b2e6d363f6ad1ffefaef4ec3fc3a8e9b
ryanl <ryanlimadev@gmail.com>
2022-09-07 11:58:04 -0300

    finalizando projeto

--- app/src/main/java/com/rlds/preferenciadousuario/MainActivity.kt
+++ app/src/main/java/com/rlds/preferenciadousuario/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.rlds.preferenciadousuario
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
 import com.rlds.preferenciadousuario.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private  lateinit var  binding: ActivityMainBinding
+    private  var  cor = ""
+    companion object{
+        const val ARQUIVO_PREFERENCIAS = "arquivoPreferencias"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -13,16 +20,58 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.hide()
 
         binding.cor1.setOnClickListener {
+            cor = "#FF03DAC5"
+            salvar(cor)
+
 
         }
         binding.cor2.setOnClickListener {
+            cor = "#FFBB86FC"
+            salvar(cor)
 
         }
         binding.cor3.setOnClickListener {
+            cor = "#EC0404"
+            salvar(cor)
 
         }
         binding.cor4.setOnClickListener {
+            cor = "#03A9F4"
+            salvar(cor)
 
+        }
+    }
+    private fun salvar(cor:String){
+        binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
+        binding.btTrocarFundo.setOnClickListener {view ->
+            val preferencias = getSharedPreferences(ARQUIVO_PREFERENCIAS, MODE_PRIVATE)
+            val editor = preferencias.edit()
+            editor.putString("cor",cor)
+            editor.apply()
+            snackBar(view)
+        }
+
+
+    }
+    private fun snackBar(view:View){
+        val snabar = Snackbar.make(view,"Cor de fundo alterada com sucesso!",
+            Snackbar.LENGTH_INDEFINITE )
+        snabar.setAction("Ok"){
+
+        }
+        snabar.setActionTextColor(Color.BLUE)
+        snabar.setBackgroundTint(Color.WHITE)
+        snabar.setTextColor(Color.BLACK)
+        snabar.show()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val preferencias = getSharedPreferences(ARQUIVO_PREFERENCIAS, MODE_PRIVATE)
+        val cor = preferencias.getString("cor", "")
+        if(cor!!.isNotEmpty()){
+            binding.layoutPrincipal.setBackgroundColor(Color.parseColor(cor))
         }
     }
 }
