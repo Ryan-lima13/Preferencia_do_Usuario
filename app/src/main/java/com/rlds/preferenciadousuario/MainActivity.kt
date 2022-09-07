a3ab98245a194e52cc111501490d6d4fc96339ee
ryanl <ryanlimadev@gmail.com>
2022-09-07 11:32:02 -0300

    criando tela principal

--- app/src/main/java/com/rlds/preferenciadousuario/MainActivity.kt
+++ app/src/main/java/com/rlds/preferenciadousuario/MainActivity.kt
@@ -2,10 +2,27 @@ package com.rlds.preferenciadousuario
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rlds.preferenciadousuario.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar!!.hide()
+
+        binding.cor1.setOnClickListener {
+
+        }
+        binding.cor2.setOnClickListener {
+
+        }
+        binding.cor3.setOnClickListener {
+
+        }
+        binding.cor4.setOnClickListener {
+
+        }
     }
 }
